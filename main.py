b0d1e6f8b641009edb93e38285cd67b68d5f14fe
Stulu <58867625+stulu08@users.noreply.github.com>
2021-01-06 20:53:51 +0100

    update
    update

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+''' 
+URL of the archive web-page which provides link to 
+all video lectures. It would have been tiring to 
+download each video manually. 
+In this example, we first crawl the webpage to extract 
+all the links and then download videos. 
+'''
+
+# specify the URL of the archive here
+archive_url = "http://www-personal.umich.edu/~csev/books/py4inf/media/"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def get_video_links():
+    # create response object
+    r = requests.get(archive_url)
+
+    # create beautiful-soup object
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    # find all links on web-page
+    links = soup.findAll('a')
+
+    # filter the link sending with .mp4
+    video_links = [archive_url + link['href'] for link in links if link['href'].endswith('mp4')]
+
+    return video_links
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def download_video_series(video_links):
+    for link in video_links:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        '''iterate through all links in video_links 
+        and download them one by one'''
+
+        # obtain filename by splitting url and getting
+        # last string
+        file_name = link.split('/')[-1]
+
+        print("Downloading file:%s" % file_name)
+
+        # create response object
+        r = requests.get(link, stream=True)
+
+        # download started
+        with open(file_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 1024):
+                if chunk:
+                    f.write(chunk)
+
+        print("%s downloaded!\n" % file_name)
+
+    print("All videos downloaded!")
+    return
+
+
+if __name__ == "__main__":
+    # getting all video links
+    video_links = get_video_links()
+
+    # download all videos
+    download_video_series(video_links)
+
+
+
